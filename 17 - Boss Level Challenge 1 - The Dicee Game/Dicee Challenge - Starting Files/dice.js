76328685904547f172c0b3e002a631a4f6ce0a51
RahulTest164 <rahul.test.ground@gmail.com>
2024-10-04 16:20:14 +0530

    Drum Kit Done

--- 17 - Boss Level Challenge 1 - The Dicee Game/Dicee Challenge - Starting Files/dice.js
+++ 17 - Boss Level Challenge 1 - The Dicee Game/Dicee Challenge - Starting Files/dice.js
@@ -4,11 +4,9 @@ document.querySelector(".dice .img1").setAttribute("src",p1);
 document.querySelector(".dice .img2").setAttribute("src",p2);
 
 if(p1>p2){
-    document.querySelector("h1").textContent = "Player 1 Wins!";
-    document.querySelector("h1").appendChild(document.createElement("img"));
-    document.querySelector("h1>img").setAttribute("src",p1);
+    document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
 }else if(p2>p1){
-    document.querySelector("h1").textContent = "Player 2 Wins!";
+    document.querySelector("h1").textContent = "🚩 Player 2 Wins!";
 } else {
     document.querySelector("h1").textContent = "Refresh Me";
 }
---
+++ 18 - Advanced Javascript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -0,0 +1,49 @@
+// document.querySelector("button").addEventListener("click", handleClick);
+
+// function handleClick(){
+//     alert("I got clicked.");
+// }
+
+for (var i=0; i< document.querySelectorAll(".drum").length; i++ ){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){handleClick(this)});
+    
+}
+
+// document.querySelectorAll("button")[0].addEventListener("click", handleClick);
+var delayInMilliseconds = 1000; //1 second
+function handleClick(element){
+    var audio;
+    var ele = element.innerText;
+    switch(ele){
+        case 'w':
+            audio = new Audio("./sounds/crash.mp3")
+            break;
+        case 'a':
+             audio = new Audio("./sounds/kick-bass.mp3")
+            break;
+        case 's':
+             audio = new Audio("./sounds/snare.mp3")
+            break; 
+        case 'd':
+             audio = new Audio("./sounds/tom-1.mp3")
+            break; 
+        case 'j':
+             audio = new Audio("./sounds/tom-2.mp3")
+            break; 
+        case 'k':
+             audio = new Audio("./sounds/tom-3.mp3")
+            break;     
+        case 'l':
+             audio = new Audio("./sounds/tom-4.mp3")
+            break;      
+
+    }
+    audio.play();
+    // this.style.color = "white";
+    
+    
+    // setTimeout(function() {
+    //     ele.style.color = "red";//your code to be executed after 1 second
+    // }, delayInMilliseconds);
+
+}
